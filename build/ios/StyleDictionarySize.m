4341551025c1f52bc7281b8aebe7fc9e222f13e9
Anvesh Dunna <anvesh.dunna@gmail.com>
2022-12-14 19:12:40 +0530

    Style dictionary added

---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Wed, 14 Dec 2022 13:41:18 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
